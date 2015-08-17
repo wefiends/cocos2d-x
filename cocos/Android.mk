ea0e2153b08ede2a1810527281ea9eb10e167c5a
Nicholas Barrios <nick@wefiends.com>
2015-08-17 12:12:41 -0400

    cocos/Android.mk: Remove static libs we are not using, to reduce compile times.

--- cocos/Android.mk
+++ cocos/Android.mk
@@ -289,13 +289,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos2dx_static
 LOCAL_MODULE_FILENAME := libcocos2d
 
-LOCAL_STATIC_LIBRARIES := cocostudio_static
-LOCAL_STATIC_LIBRARIES += cocosbuilder_static
-LOCAL_STATIC_LIBRARIES += cocos3d_static
-LOCAL_STATIC_LIBRARIES += spine_static
-LOCAL_STATIC_LIBRARIES += cocos_network_static
-LOCAL_STATIC_LIBRARIES += audioengine_static
-LOCAL_STATIC_LIBRARIES += bullet_static
+LOCAL_STATIC_LIBRARIES := cocos3d_static
 
 include $(BUILD_STATIC_LIBRARY)
 #==============================================================
@@ -308,15 +302,7 @@ $(call import-module,tiff/prebuilt/android)
 $(call import-module,webp/prebuilt/android)
 $(call import-module,chipmunk/prebuilt/android)
 $(call import-module,3d)
-$(call import-module,audio/android)
-$(call import-module,editor-support/cocosbuilder)
-$(call import-module,editor-support/cocostudio)
-$(call import-module,editor-support/spine)
-$(call import-module,network)
-$(call import-module,ui)
-$(call import-module,extensions)
 $(call import-module,Box2D)
-$(call import-module,bullet)
 $(call import-module,recast)
 $(call import-module,curl/prebuilt/android)
 $(call import-module,websockets/prebuilt/android)
