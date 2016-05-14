fe5c21645cdb1134e457756ebc932c345be05397
Nicholas Barrios <nick@wefiends.com>
2016-05-14 18:25:07 -0400

    Remove forced libchipmunk.lib include.

--- cocos/platform/win32/CCPlatformDefine-win32.h
+++ cocos/platform/win32/CCPlatformDefine-win32.h
@@ -65,7 +65,6 @@ THE SOFTWARE.
 #pragma comment(lib,"libjpeg-2015.lib")
 #pragma comment(lib,"libtiff-2015.lib")
 #pragma comment(lib,"glfw3-2015.lib")
-#pragma comment(lib,"libchipmunk-2015.lib")
 #else
 #pragma comment(lib,"libpng.lib")
 #pragma comment(lib,"libjpeg.lib")
