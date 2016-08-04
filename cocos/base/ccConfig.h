3fedef07faff2eb8de9f61e611fb498cec155737
Nicholas Barrios <nick@wefiends.com>
2016-08-03 22:46:18 -0400

    Remove Bullet, Recast, and Extensions from iOS lib project.

--- cocos/base/ccConfig.h
+++ cocos/base/ccConfig.h
@@ -296,7 +296,7 @@ THE SOFTWARE.
 /** Support JPEG or not. If your application don't use jpeg format picture, you can undefine this macro to save package size.
  */
 #ifndef CC_USE_JPEG
-#define CC_USE_JPEG  1
+#define CC_USE_JPEG  0
 #endif // CC_USE_JPEG
 
 /** Support TIFF or not. If your application don't use TIFF format picture, you can undefine this macro to save package size.
@@ -309,7 +309,7 @@ THE SOFTWARE.
  */
 #ifndef CC_USE_WEBP
 #if (CC_TARGET_PLATFORM != CC_PLATFORM_WINRT)
-#define CC_USE_WEBP  0
+#define CC_USE_WEBP  1
 #endif
 #endif // CC_USE_WEBP
 
